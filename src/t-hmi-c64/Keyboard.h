51ac412a72df5d77c5dda77a222b4ccdc5b2538c
gordondu <gordondu@tencent.com>
2024-07-23 17:21:04 +0800

    update

--- src/t-hmi-c64/Keyboard.h
+++ src/t-hmi-c64/Keyboard.h
@@ -5,13 +5,26 @@
 #include <unordered_map>
 #include <mutex>
 
-#include "SDL_scancode.h"
-
 #define KEY_SIZE 0xFF
 #define INLINE IRAM_ATTR inline __attribute__((always_inline, flatten))
-#define SDLK_SCANCODE_MASK (1 << 30)
-#define SDL_SCANCODE_TO_KEYCODE(X) (X | SDLK_SCANCODE_MASK)
-typedef int32_t SDL_Keycode;
+
+typedef enum code {
+  CODE_DELETE, CODE_RETURN, CODE_CRSRRT,    CODE_F7,    CODE_F1,     CODE_F3,    CODE_F5,    CODE_CRSRDN,
+  CODE_3,      CODE_W,      CODE_A,         CODE_4,     CODE_Z,      CODE_S,     CODE_E,     CODE_LSHIFT,
+  CODE_5,      CODE_R,      CODE_D,         CODE_6,     CODE_C,      CODE_F,     CODE_T,     CODE_X,
+  CODE_7,      CODE_Y,      CODE_G,         CODE_8,     CODE_B,      CODE_H,     CODE_U,     CODE_V,
+  CODE_9,      CODE_I,      CODE_J,         CODE_0,     CODE_M,      CODE_K,     CODE_O,     CODE_N,
+  CODE_PLUS,   CODE_P,      CODE_L,         CODE_MINUS, CODE_PERIOD, CODE_COLON, CODE_AT,    CODE_COMMA,
+  CODE_POUND,  CODE_STAR,   CODE_SEMICOLON, CODE_HOME,  CODE_RSHIFT, CODE_EUQAL, CODE_CARET, CODE_SLASH,
+  CODE_1,      CODE_BACK,   CODE_CTRL,      CODE_2,     CODE_SPACE,  CODE_GUI,   CODE_Q,     CODE_STOP, CODE_INVALID,
+} CODE;
+
+const CODE kb_map[4][14] = {
+    {CODE_INVALID, CODE_1, CODE_2, CODE_3, CODE_4, CODE_5, CODE_6, CODE_7, CODE_8, CODE_9, CODE_0, CODE_MINUS, CODE_EUQAL, CODE_DELETE},
+    {CODE_INVALID, CODE_Q, CODE_W, CODE_E, CODE_R, CODE_T, CODE_Y, CODE_U, CODE_I, CODE_O, CODE_P, CODE_INVALID, CODE_INVALID, CODE_INVALID},
+    {CODE_INVALID, CODE_INVALID, CODE_A, CODE_S, CODE_D, CODE_F, CODE_G, CODE_H, CODE_J, CODE_K, CODE_L, CODE_SEMICOLON, CODE_INVALID, CODE_RETURN},
+    {CODE_INVALID, CODE_INVALID, CODE_INVALID, CODE_Z, CODE_X, CODE_C, CODE_V, CODE_B, CODE_N, CODE_M, CODE_COMMA, CODE_PERIOD, CODE_SLASH, CODE_SPACE}};
+
 
 static const uint8_t C64JOYUP = 0;
 static const uint8_t C64JOYDOWN = 1;
@@ -28,15 +41,15 @@ private:
   std::atomic<bool> reset_;
   bool retval_ = true;
   /* keyboard mappings */
-  std::unordered_map<SDL_Keycode, std::pair<int, int>> keymap_;
-  std::unordered_map<char, std::vector<SDL_Keycode>> charmap_;
+  std::unordered_map<CODE, std::pair<int, int>> keymap_;
+  std::unordered_map<char, std::vector<CODE>> charmap_;
   enum kKeyEvent
   {
     kPress,
     kRelease,
   };
   /* key events */
-  std::queue<std::pair<kKeyEvent, SDL_Keycode>> key_event_queue_;
+  std::queue<std::pair<kKeyEvent, CODE>> key_event_queue_;
 
   uint8_t joystickValue;
   bool joystickFire;
@@ -47,8 +60,8 @@ public:
   INLINE bool reset() { return reset_; };
   void init();
   void handleKeyboard();
-  void handleKeyUp(SDL_Keycode k);
-  void handleKeyDown(SDL_Keycode k);
+  void handleKeyUp(CODE k);
+  void handleKeyDown(CODE k);
   void typeCharacter(char c);
 
   INLINE uint8_t getJoyStickValue(bool port2, uint8_t dc00, uint8_t dc02)
